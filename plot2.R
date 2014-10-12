1312e1c7924e8cc7d334bd66ed5c21ff2b29fd28
unknown <elarsen@SEALT34ECC73.na.blkint.com>
2014-10-12 15:34:25 -0700

    Plot 2 in good shape

--- plot2.R
+++ plot2.R
@@ -1,20 +1,23 @@
 ## Draws the plot
 
-plot1<- function(){
+plot2<- function(){
   ##Reads in the full file
   data<-read.table("household_power_consumption.txt", sep =";", header = TRUE, nrows=70000, stringsAsFactors=FALSE)
-
+  
   ##subsets the data for the dates we care about
-
+  
   datasub<-data[data$Date %in% c("2/1/2007","2/2/2007"),]
-  print(datasub[1:10,])
+  print(dim(datasub))
+  png(file="plot2.png", height=480, width = 480)
+
+x<-strptime(paste(as.Date(datasub$Date,format="%m/%d/%Y"),datasub$Time), "%Y-%m-%d %H:%M:%S")
 
   ##Draws the plot as requested
-  hist(as(datasub$Global_active_power,"numeric"), col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power")
+  plot(strptime(paste(as.Date(datasub$Date,format="%m/%d/%Y"),datasub$Time), "%Y-%m-%d %H:%M:%S"),
+       as(datasub$Global_active_power,"numeric"),
+       ylab="Global Active Power (kilowatts)", xlab="",type="l", main="")
   
-  ##Copy plot fomr screen device to a file
-  
-  dev.copy(png, file="plot1.png")
-  dev.off()
 
+  dev.off()
+  
 }
